16dbb635bf5aa662d9d812da54af1bd3abe2960a
ChromaChroma <jona.beer@gmail.com>
2024-06-06 18:11:47 +0200

    Add shell scripts for manual testing

---
+++ debug-test.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+
+#cd ..
+#cargo build
+#cd .scripts
+
+../target/release/splashsurf.exe reconstruct ../data/cube_2366_particles.vtk \
+ --output-dir ../data/converted \
+ -r=0.025 -l=2.0 -c=0.5 -t=0.6 \
+ --subdomain-grid=on \
+ --mesh-cleanup=on \
+ --mesh-smoothing-weights=on \
+ --mesh-smoothing-iters=25 \
+ --normals=on \
+ --normals-smoothing-iters=10
---
+++ profile.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+cd ..
+
+# Build CLI tool
+#cargo build
+
+echo "Script executed from: ${PWD}"
+
+#--bin=splashsurf
+cargo flamegraph --bin="./target/release/splashsurf.exe" \
+ -- reconstruct ../data/extra/DoubleDamBreakWithSphere/vtk/ParticleData_Fluid_{}.vtk \
+ --start-index=135 --end-index=258 \
+ --output-dir ../data/extra/DoubleDamBreakWithSphere/surface-output \
+ --quiet \
+ -r=0.025 -l=2.0 -c=0.5 -t=0.6 \
+ --subdomain-grid=on \
+ --mesh-cleanup=on \
+ --mesh-smoothing-weights=on \
+ --mesh-smoothing-iters=25 \
+ --normals=on \
+ --normals-smoothing-iters=10  --mt-files=on --mt-particles=off
+
+ flamegraph -- ./target/release/splashsurf.exe reconstruct ./data/cube_2366_particles.vtk  --start-index=135 --end-index=258   --quiet  -r=0.025 -l=2.0 -c=0.5 -t=0.6  --subdomain-grid=on  --mesh-cleanup=on  --mesh-smoothing-weights=on  --mesh-smoothing-iters=25  --normals=on  --normals-smoothing-iters=10  --mt-files=on --mt-particles=off
