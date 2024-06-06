d9048163df2e07f55ae0cb1957b4a0c0075bf871
ChromaChroma <jona.beer@gmail.com>
2024-06-07 00:51:39 +0200

    Run Scripts (can be configured as jetbrains run configs)

---
+++ run-release-multi.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+cargo run --package splashsurf --bin splashsurf --release \
+  -- \
+  reconstruct ./data/DoubleDamBreakWithSphere/vtk/ParticleData_Fluid_{}.vtk \
+   --start-index=0 --end-index=258 \
+   --output-dir ./data/DoubleDamBreakWithSphere/surface-output \
+  -r=0.025 -l=2.0 -c=0.5 -t=0.6 \
+  --subdomain-grid=on \
+  --mesh-cleanup=on  --mesh-smoothing-weights=on   --mesh-smoothing-iters=25 \
+  --normals=on --normals-smoothing-iters=10 \
+  --mt-files=on --mt-particles=off # Multiple frames parallel processed
