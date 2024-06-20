2d2f391ce4b11fa3da5374c9cc5c8d7854912cc7
ChromaChroma <jona.beer@gmail.com>
2024-06-20 15:48:25 +0200

    Implemented running demo function

--- splashsurf_lib/src/gpu/dense_subdomains.rs
+++ splashsurf_lib/src/gpu/dense_subdomains.rs
@@ -1,10 +1,14 @@
+use std::ptr;
 use std::sync::{Arc, Mutex};
 use log::info;
 use nalgebra::Vector3;
+use opencl3::command_queue::{cl_event, CL_NON_BLOCKING, CL_QUEUE_ON_DEVICE, CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE, CL_QUEUE_PROFILING_ENABLE, CommandQueue};
 use opencl3::context::Context;
-use opencl3::device::{CL_DEVICE_TYPE_GPU, Device};
-use opencl3::kernel::Kernel;
+use opencl3::device::{CL_DEVICE_TYPE_GPU, cl_double, Device};
+use opencl3::kernel::{ExecuteKernel, Kernel};
+use opencl3::memory::{Buffer, CL_MEM_READ_WRITE};
 use opencl3::program::{CL_STD_2_0, Program};
+use opencl3::types::cl_int;
 use crate::{Index, OpenCLData, profile, Real};
 use crate::dense_subdomains::{ParametersSubdomainGrid, subdomain_classification, Subdomains};
 use crate::gpu::kernel::init_kernels;
@@ -20,6 +24,49 @@ pub(crate) fn decomposition<
 ) -> Result<Subdomains<I>, anyhow::Error> {
     profile!("GPU::decomposition");
     info!("GPU::Starting classification of particles into subdomains.");
+
+    // Create a command_queue on the Context's device
+    let queue = CommandQueue::create_default(
+        &ocl_data.lock().unwrap().context, CL_QUEUE_PROFILING_ENABLE,
+    ).expect("CommandQueue::create_default failed");
+
+    const WORKGROUP_SIZE: usize = 2048;
+
+
+    // Output Buffers
+    // let levelset_grid_f64_arr: &[cl_double] = &*convert_slice_to_cl_double(&levelset_grid_f64);
+
+    let mut output_buffer = unsafe {
+        Buffer::<cl_int>::create(&ocl_data.lock().unwrap().context, CL_MEM_READ_WRITE, WORKGROUP_SIZE, ptr::null_mut()).expect("Could not create output_buffer")
+    };
+    let _output_buffer_write_event = unsafe {
+        queue.enqueue_write_buffer(&mut output_buffer, CL_NON_BLOCKING, 0, &[0; WORKGROUP_SIZE], &[]).expect("Could not enqueue output_buffer")
+    };
+
+    let kernel_event = unsafe {
+        ExecuteKernel::new(&ocl_data.lock().unwrap().kernel)
+            .set_arg(&output_buffer)
+            .set_global_work_size(WORKGROUP_SIZE)
+            .set_wait_event(&_output_buffer_write_event)
+            .enqueue_nd_range(&queue)
+            .expect("Could not run Kernel")
+    };
+
+    let mut events: Vec<cl_event> = Vec::default();
+    events.push(kernel_event.get());
+
+    let mut results = &mut vec![0 as cl_int; WORKGROUP_SIZE];
+    let read_event = unsafe { queue.enqueue_read_buffer(&output_buffer, CL_NON_BLOCKING, 0, &mut results, &events).expect("Could not enqueue result read buffer") };
+    // Wait for the read_event to complete.
+    read_event.wait().expect("Could not read event for retrieveing data from gpu bufer");
+
+    println!("{:?}", results);
+
+    // Calculate the kernel duration, from the kernel_event
+    let start_time = kernel_event.profiling_command_start().unwrap();
+    let end_time = kernel_event.profiling_command_end().unwrap();
+    let duration = end_time - start_time;
+    println!("kernel execution duration (ns): {}", duration);
 
 
     // init_kernels().expect(" Gpu failed initializing");
